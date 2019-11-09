04c96b825f862d042239221377c9de7598decde3
gdubey123 <gaurav.dubey123@gmail.com>
2019-11-09 19:48:28 +0530

    Create outputs.tf

---
+++ terraform/aws/modules/symmetrical-spork/outputs.tf
@@ -0,0 +1,35 @@
+output app_security_group_id {
+  value = "${module.app_sg.app_security_group_id}"
+}
+
+output elb_security_group_id {
+  value = "${module.elb_sg.elb_security_group_id}"
+}
+
+output elb_internal_security_group_id {
+  value = "${module.elb_internal_sg.elb_security_group_id}"
+}
+
+output elb_internal_dns_name {
+  value = "${module.elb_internal.elb_dns_name}"
+}
+
+output asg_name {
+  value = "${module.lc-asg.asg_name}"
+}
+
+output r53_record_elb {
+  value = "${module.r53_record_elb.r53_record_name}"
+}
+
+output app_iam_user_name {
+  value = "${module.app_iam_user.app_iam_user_name}"
+}
+
+output app_iam_user_access_key {
+  value = "${module.app_iam_user.app_iam_user_access_key}"
+}
+
+output app_iam_user_secret_key {
+  value = "${module.app_iam_user.app_iam_user_secret_key}"
+}
