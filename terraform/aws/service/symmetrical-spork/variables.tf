704f238bd53603a639919fc39a6d2e548f79b101
gdubey123 <gaurav.dubey123@gmail.com>
2019-11-09 20:20:37 +0530

    Create variables.tf

---
+++ terraform/aws/service/symmetrical-spork/variables.tf
@@ -0,0 +1,14 @@
+variable asg_suffix {
+  type = "string"
+  default = ""
+}
+
+variable min_elb_capacity {
+  type = "string"
+  default = "0"
+}
+
+variable wait_for_capacity_timeout {
+  type = "string"
+  default = "50m"
+}
